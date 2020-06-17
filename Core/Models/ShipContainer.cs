d16ee1b37cb9e25a7f62d2ba03221f83780f47a7
Oscar <oscar.f.larsson@gmail.com>
2020-06-17 11:02:02 +0200

    Added vertical/horizontal validation for ShipContainer coordinates

--- Core/Models/ShipContainer.cs
+++ Core/Models/ShipContainer.cs
@@ -44,6 +44,23 @@ namespace Core.Models
                 throw new ShipValidationException(
                     $"Ship of type {this.ShipType.ToString()} must have {ShipType.NrOfBoxes()} coordinates! Only {length} coordinates were passed.");
             }
+
+            ValidateCoordinatesAreInLine(coordinates);
+        }
+
+        private void ValidateCoordinatesAreInLine(List<(Column column, int row)> coordinates)
+        {
+
+            var isVertical = coordinates.Select(coord => coord.column).Distinct().Count() == 1;
+
+            if (!isVertical)
+            {
+                var isHorizontal = coordinates.Select(coord => coord.row).Distinct().Count() == 1;
+                if (!isHorizontal)
+                {
+                    throw new ShipValidationException($"Ship {ShipType.ToString()} is not vertical or horizontal. Check the coordinates again!");
+                }
+            }
         }
     }
 
