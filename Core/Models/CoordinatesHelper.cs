5a722efa5d4824b2ec647a432208da81237b0b8e
Oscar <oscar.f.larsson@gmail.com>
2020-06-19 19:38:00 +0200

    Added explicit numeric value to int, to not have 0 based numbers

--- Core/Models/CoordinatesHelper.cs
+++ Core/Models/CoordinatesHelper.cs
@@ -8,16 +8,16 @@ namespace Core.Models
         public static int GetColumnCount() => 10;
         public enum Column
         {
-            A,
-            B,
-            C,
-            D,
-            E,
-            F,
-            G,
-            H,
-            I,
-            J
+            A = 1,
+            B = 2,
+            C = 3,
+            D = 4,
+            E = 5,
+            F = 6,
+            G = 7,
+            H = 8,
+            I = 9,
+            J = 10
         }
 
         public class CoordinateException : Exception
