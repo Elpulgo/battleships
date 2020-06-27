2737111b9095ae9011bc1c1bc0156faa264a5550
Oscar <oscar.f.larsson@gmail.com>
2020-06-27 20:07:15 +0200

    Able to run blazor app, and show names of ships, from Core lib. Testing

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -17,7 +17,11 @@ namespace Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+            // builder.Services.AddSingleton<Core.Models.GameBoard, Core.Models.GameBoard>();
 
             await builder.Build().RunAsync();
         }
