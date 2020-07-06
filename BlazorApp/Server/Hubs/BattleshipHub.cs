704afbe023ab7171434822e222296558bd7b73a5
Oscar <oscar.f.larsson@gmail.com>
2020-07-06 15:11:10 +0200

    Added ConnectionManager to hold dictionary of playerId and ConnectionId. Added GameModeChanged and inject ConnectionManager in BattleshipHub

--- BlazorApp/Server/Hubs/BattleshipHub.cs
+++ BlazorApp/Server/Hubs/BattleshipHub.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Core.Models;
+using BlazorApp.Server.Managers;
 
 namespace BlazorApp.Server.Hubs
 {
     public class BattleshipHub : Hub
     {
+        private readonly ConnectionManager<Player> _connectionManager;
+
+        public BattleshipHub(ConnectionManager<Player> connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
+
+        public string GetConnectionId() => Context.ConnectionId;
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task GameModeChanged(GameMode gameMode) => await Clients.All.SendAsync("GameModeChanged", gameMode);
+        public async Task GameModeChanged(GameMode gameMode)
+        {
+            Console.WriteLine("Gamemode is.." + gameMode);
+
+            await Clients.All.SendAsync("GameModeChanged", gameMode);
+        }
     }
 }
